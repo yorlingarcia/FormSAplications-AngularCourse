3d400134a5f84dfba7ec6e754fba19b8b146a9cf
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-03-01 20:09:28 -0500

    creacion del archivo de rutas

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styles: [
+  ]
+})
+export class SidemenuComponent {
+
+}

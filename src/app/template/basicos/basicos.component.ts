21b0013dc0bf8e5f869a735c4a34f75ec95b414c
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-03-01 21:30:43 -0500

    se agrego un areferencia local como atribto al tag formm, especificandole que enlace la referencia

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BasicosComponent {
-
+  guardar() {
+    console.log('submit enviado');
+  }
 }

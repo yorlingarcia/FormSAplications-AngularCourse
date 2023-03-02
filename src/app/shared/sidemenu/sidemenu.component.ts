7010b596b8a5dc76f25fc6941b215b3d09dcbdbb
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-03-01 20:48:10 -0500

    se agregaron directivas  y el routerLinActivate para agregar una clase al tag

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  texto: string;
+  ruta: string;
+}
+
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SidemenuComponent {
-
+  templateMenu: MenuItem[] = [
+    {
+      texto: 'Basicos',
+      ruta: './template/basicos',
+    },
+    {
+      texto: 'Dinamicos',
+      ruta: './template/dinamicos',
+    },
+    {
+      texto: 'Switches',
+      ruta: './template/switches',
+    },
+  ];
 }

94e0bfbcbc234696c90e30b8c56268406cae0068
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-03-03 14:54:52 -0500

    creacion de los switches

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SwitchesComponent {
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  };
 
+  terminosYCondiciones: boolean = false;
 }

dc3a3a30c9bf730833d64db12737842cfe2bd83f
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-03-04 20:52:58 -0500

    se creo un metodo para el submit validando el formulario, para mostrar las advertencias

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -19,10 +19,25 @@ export class BasicosComponent {
   // });
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['Manzana', [Validators.required, Validators.minLength(3)]],
-    precio: [0, [Validators.required, Validators.min(0)]],
-    existencias: [0, [Validators.required, Validators.min(0)]],
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    precio: [, [Validators.required, Validators.min(0)]],
+    existencias: [, [Validators.required, Validators.min(0)]],
   });
 
   constructor(private fb: FormBuilder) {}
+
+  campoValido(campo: string) {
+    return (
+      !this.miFormulario.controls[campo].valid &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+  }
 }

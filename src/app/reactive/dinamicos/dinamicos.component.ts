0c52de74506a7cb4e98b55f0a2f13e6e06c07d0f
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-03-04 22:00:01 -0500

    uso del FormBuilder para la pagina de dinamicos

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DinamicosComponent {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+  }
+
+  campoValido(campo: string) {
+    return (
+      !this.miFormulario.controls[campo].valid &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
 }

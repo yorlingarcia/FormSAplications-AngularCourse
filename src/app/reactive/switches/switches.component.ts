1b7601ce8123e48124f5d7e2565d48cf5030edcc
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-03-05 15:24:50 -0500

    actualizacion dinamica del formulario

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SwitchesComponent {
+export class SwitchesComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['F', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue],
+  });
 
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  };
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.miFormulario.reset({ ...this.persona, notificaiones: true });
+    //this.miFormulario
+    //  .get('condiciones')
+    //  ?.valueChanges.subscribe((newValue) => console.log(newValue));
+
+    this.miFormulario.valueChanges.subscribe((form) => {
+      delete form.condiciones;
+      this.persona = form;
+    });
+  }
+
+  guardar() {
+    const formValue = { ...this.miFormulario.value };
+    delete formValue.condiciones;
+    this.persona = formValue;
+  }
 }

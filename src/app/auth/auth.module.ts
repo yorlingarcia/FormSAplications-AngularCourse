27e01807c56df83a771461af27557c6cd568ba26
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-03-28 14:38:18 -0500

    validacion del nombre

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegistroComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [LoginComponent, RegistroComponent],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,7 +1,30 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
 })
-export class RegistroComponent {}
+export class RegistroComponent {
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [
+      '',
+      [Validators.required, Validators.pattern(this.nombreApellidoPattern)],
+    ],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  campoNoValido(campo: string) {
+    return (
+      this.miFormulario.get(campo)?.invalid &&
+      this.miFormulario.get(campo)?.touched
+    );
+  }
+
+  submitFormulario() {
+    console.log(this.miFormulario);
+    this.miFormulario.markAllAsTouched();
+  }
+}

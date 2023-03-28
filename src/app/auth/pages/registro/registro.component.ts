0b3b0078b017d519d1906e2e4bc53f000c494f6f
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-03-28 15:14:29 -0500

    validacon del email y el usarname

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,20 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
   nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+
+  noPuedeSerStrider(argumento: any) {
+    console.log(argumento);
+  }
+
   miFormulario: FormGroup = this.fb.group({
     nombre: [
       '',
       [Validators.required, Validators.pattern(this.nombreApellidoPattern)],
     ],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+    userName: ['', [Validators.required]],
   });
 
   constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.miFormulario.reset({
+      nombre: 'Yorlin Garcia',
+      email: 'test@test.com',
+    });
+  }
 
   campoNoValido(campo: string) {
     return (

be55073c15a4021b2d7be9bb3877326da0c7f0df
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-04-03 23:26:38 -0500

    uso del html para mostrar los errores correspondientes

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -60,4 +60,23 @@ export class RegistroComponent implements OnInit {
     console.log(this.miFormulario);
     this.miFormulario.markAllAsTouched();
   }
+
+  emailRequired() {
+    return (
+      this.miFormulario.get('email')?.errors?.['required'] &&
+      this.miFormulario.get('email')?.touched
+    );
+  }
+  emailFormato() {
+    return (
+      this.miFormulario.get('email')?.errors?.['pattern'] &&
+      this.miFormulario.get('email')?.touched
+    );
+  }
+  emailTomado() {
+    return (
+      this.miFormulario.get('email')?.errors?.['emailTomado'] &&
+      this.miFormulario.get('email')?.touched
+    );
+  }
 }
--- src/app/shared/validator/email-validator.service.ts
+++ src/app/shared/validator/email-validator.service.ts
@@ -22,7 +22,7 @@ export class EmailValidatorService implements AsyncValidator {
     return this.http
       .get<any[]>(`http://localhost:3000/usuarios?q=${email}`)
       .pipe(
-        delay(3000),
+        // delay(3000),
         map((resp) => {
           return resp.length === 0 ? null : { emailTomado: true };
         })
